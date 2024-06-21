638091be0ca75811afb8d27ec3f9a945ddcd9433
rfyanez <cyclope35@gmail.com>
2024-06-21 12:48:05 -0600

    Update StudentServiceImpl.java
    Clase StudentServiceImpl Metodo findAllAndShowIfHaveAPopularSubject Se realizo su implementacion.

--- src/main/java/com/imsoftware/students/service/impl/StudentServiceImpl.java
+++ src/main/java/com/imsoftware/students/service/impl/StudentServiceImpl.java
@@ -40,7 +40,47 @@ public class StudentServiceImpl implements IStudentService {
 	public Collection<StudentDTO> findAllAndShowIfHaveAPopularSubject() {
 		// TODO Obtener la lista de todos los estudiantes e indicar la materia más concurrida existentes en la BD e
 		// indicar si el estudiante cursa o no la materia más concurrida registrado en la BD.
-		return null;
+		
+		final Map<String, Integer> m = new HashMap<String, Integer>();
+		
+		final List<StudentDTO> ss = studentRepository.findAll().stream().map(new Function<Student, StudentDTO>() {
+			@Override
+			public StudentDTO apply(Student student) {
+				List<String> programmingLanguagesKnowAbout = student.getSubjects().stream()
+						.map(pl -> new String(pl.getName())).collect(Collectors.toList());
+				
+				programmingLanguagesKnowAbout.forEach(s -> {
+					if(m.containsKey(s))
+						m.put(s, m.get(s) + 1);
+					else
+						m.put(s, 1);
+				});
+				
+				return new StudentDTO(student.getName(), programmingLanguagesKnowAbout);
+			}
+
+		}).collect(Collectors.toList());
+		
+		m.entrySet().stream()
+		   .sorted(Map.Entry.<String, Integer> comparingByValue().reversed());
+		
+		final String mm =  m.keySet().stream()
+				.collect(Collectors.toList()).get(0);
+		
+		return ss.stream().map(new Function<StudentDTO, StudentDTO>() {
+			@Override
+			public StudentDTO apply(StudentDTO dto) {
+				AtomicBoolean ism = new AtomicBoolean(false);
+				dto.getCurrentSubject().forEach(sb -> {
+					ism.set(mm.equals(sb));
+				});
+				
+				if(ism.get())
+					return new StudentDTO(dto.getStudentName(), true);
+				
+				return dto;
+			}
+		}).collect(Collectors.toList());
 	}
 
 }
